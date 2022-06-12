60c8f25dfdd0f81c4d1fff61efd47b52a18bd5d2
Liam Carney <liammitchelcarney.au@gmail.com>
2022-06-13 02:48:41 +1000

    chore: updated missing dependencies for Apache Commons

--- src/main/java/com/mith/EmojiChat/EmojiChatCommand.java
+++ src/main/java/com/mith/EmojiChat/EmojiChatCommand.java
@@ -1,7 +1,7 @@
 package com.mith.EmojiChat;
 
 import com.mith.UnicodeApi.Objects.Unicode;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
